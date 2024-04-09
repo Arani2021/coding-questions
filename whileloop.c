d49534f2c9d746ed07a4e64eecb410f929f52bf8
Arani Dasgupta <142140010+Arani2021@users.noreply.github.com>
2024-04-09 23:52:05 +0530

    Delete whileloop.c

--- whileloop.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int i = 1;
-    while(i<=10){//যখন বেশি জিনিস জানা থাকে না তখন while loop use করা হয়।
-        printf("%d ",i);
-        i++;
-    }
-    return 0;
-}
