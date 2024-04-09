6072f1cfe1119914d8e1c5cba8de96f047264081
Arani Dasgupta <142140010+Arani2021@users.noreply.github.com>
2024-04-10 00:00:50 +0530

    Delete predict5.c

--- predict5.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    while('a'<'b')//ascii value of a=97,b=98
-    printf("malayalam is a palindrom");
-    return 0;
-}
-//97 is always less than 98 তাই লুপটা শেষ হবে না। এটি infinite loop
